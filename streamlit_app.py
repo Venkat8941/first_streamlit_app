6a061a3603d3a37cfbd5f669f5d0653c29fe55b6
Venkat8941 <96866801+Venkat8941@users.noreply.github.com>
2023-03-03 11:54:02 +0530

    NEW_RESPONCE

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#new section to dispaly fruityvice api responce
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
